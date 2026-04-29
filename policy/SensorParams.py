8fd4b96b60e2be781bc55480005d309a7b98c6e3
kimh060612 <kimh060612@naver.com>
2026-04-29 22:08:10 +0900

    fine parameter space

--- policy/SensorParams.py
+++ policy/SensorParams.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class SensorParamSpace:
-    exposure_values: Tuple[float, ...] = (0.001, 0.002, 0.004, 0.008, 0.016)
-    iso_values: Tuple[int, ...] = (100, 400, 800, 1600, 3200)
+    exposure_values: Tuple[float, ...] = (0.001, 0.002, 0.004, 0.006, 0.008, 0.012, 0.016)
+    iso_values: Tuple[int, ...] = (100, 400, 600, 800, 1200, 1600, 2400, 3200)
