2486c32afce856e5a48d740053bd1838c542e177
kimh060612 <kimh060612@naver.com>
2026-05-11 14:18:31 +0900

    AE fixed speed

--- main_auto_exposure.py
+++ main_auto_exposure.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
     set_deterministic(RANDOM_SEED)
     reward_function = select_reward_function(args.reward_type)
     context_light = [200, 1000, 3000, 6000, 9000]
-    context_agent_speed = [1.5, 2.0, 1.5, 2.0, 1.5]
+    context_agent_speed = [2.0, 2.0, 2.0, 2.0, 2.0]
     # [0.2, 0.5, 1.0, 1.5, 2.0]
     trajectory = build_default_context_trajectory(
         light_values=[1000, 1000, 1000, 1000, 1000],
