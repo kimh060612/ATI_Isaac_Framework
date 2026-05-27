dad7ac7a05622f4a9fc09205b1018c229ff4fa7c
kimh060612 <kimh060612@naver.com>
2026-05-27 21:16:38 +0900

    ISO fixed

--- policy/SensorParams.py
+++ policy/SensorParams.py
@@ -4,4 +4,4 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class SensorParamSpace:
     exposure_values: Tuple[float, ...] = (0.001, 0.002, 0.004, 0.006, 0.008, 0.012, 0.016)
-    iso_values: Tuple[int, ...] = (100, 400, 600, 800, 1200, 1600, 2400, 3200)
+    iso_values: Tuple[int, ...] = (100, 200, 400, 800, 1600)
